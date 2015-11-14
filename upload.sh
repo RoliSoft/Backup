6d5ab2e8e3ac6b0302e3d5240a7fab2cd803bbcd
RoliSoft <rolisoft@gmail.com>
2015-11-14 16:13:37 +0200

    Fixed uploader.

--- upload.sh
+++ upload.sh
@@ -6,15 +6,20 @@ find "arch" -mindepth 1 -maxdepth 1 -type f | while read -r file; do
 	echo "uploading archive $file..."
 	
 	name=$(echo "$file" | sed 's/\.\./\//g' | sed 's/^arch\///')
-	megadir=${name%/*}
+	dstdir=${name%/*}
 	
 	# upload to google
 	
-	( rclone copy "$file" "gdjk:Backup/Automatic/$name" )&
+	(
+		rclone -v copy "$file" "gdjk:Backup/Automatic/$dstdir" 2>&1 | awk '{ print "gdrive: " $0 }'
+	)&
 	
 	# upload to mega
 	
-	( megamkdir "/Root/Backup/Automatic/$megadir"; megaput --path "/Root/Backup/Automatic/$name" $file )&
+	(
+		megamkdir "/Root/Backup/Automatic/$dstdir" 2>&1 | tr '\r' '\n' | awk '{ print "mega: " $0 }';
+		megaput --path "/Root/Backup/Automatic/$dstdir" $file 2>&1 | tr '\r' '\n' | awk '{ print "mega: " $0 }'
+	)&
 	
 	# wait for all uploads to finish
 	
@@ -22,5 +27,5 @@ find "arch" -mindepth 1 -maxdepth 1 -type f | while read -r file; do
 	
 	# remove file
 	
-	#rm "$file"
+	rm "$file"
 done
